aaf425da33dce33942d086d7edd39434d63f6a2c
Vimukthi <vbwickramasinghe@gmail.com>
2017-09-18 22:13:08 +0200

    Initial commit

---
+++ src/main/java/com/vimukthi/spring5test/Spring5testApplication.java
@@ -0,0 +1,12 @@
+package com.vimukthi.spring5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5testApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vimukthi/spring5test/Spring5testApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vimukthi.spring5test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5testApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
